1d5a915c436ae41089710726cb27054e9f601f04
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2025-03-05 19:13:39 -0500

    Adjust webpack config to support sass

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,15 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
       {
-        test: /\.('woff|woff2|eot|ttf|otf')$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],

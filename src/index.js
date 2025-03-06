cb1e6b3c6ee9d993dd9d8b5661ce76234e1e7e6a
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2025-03-06 18:41:25 -0500

    Install and import font Poppins to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-import "./styles.scss"
+import "./styles.scss";
+import "@fontsource/poppins/200.css"; // Light
+import "@fontsource/poppins/400.css"; // Semi-bold
+import "@fontsource/poppins/600.css"; // Bold
